4f63b7538168e3a15ba1526e2da9ab19c5aea350
Jakob Gimpelj <22889949+Gimpely@users.noreply.github.com>
2024-01-10 10:55:53 +0100

    Add files via upload

--- beyondHelp/lidar_scan.py
+++ beyondHelp/lidar_scan.py
@@ -2,7 +2,7 @@
 import numpy as np
 import rospy
 from sensor_msgs.msg import LaserScan
-from std_msgs.msg import Int32
+from std_msgs.msg import Int32, Float32
 #from std_msgs.msg import String Float64MultiArray
 import time
 import csv
@@ -13,80 +13,66 @@ import rospkg
 from datetime import datetime
 
 
+
 rospack = rospkg.RosPack()
 #pub = rospy.Publisher('/lidar_map', Float64MultiArray, queue_size=10)
 print('Values in meters.')
 
+# precompute mask for the mask_outside_circle function
+radius = 13
+y, x = np.ogrid[-radius:radius+1, -radius:radius+1]
+mask = x**2 + y**2 > radius**2 
+
 pubs = {
-    'A1': rospy.Publisher('sensor/A1', Int32, queue_size=10),
-    'A2': rospy.Publisher('sensor/A2', Int32, queue_size=10),
-    'B1': rospy.Publisher('sensor/B1', Int32, queue_size=10),
-    'B2': rospy.Publisher('sensor/B2', Int32, queue_size=10),
-    'C1': rospy.Publisher('sensor/C1', Int32, queue_size=10),
-    'C2': rospy.Publisher('sensor/C2', Int32, queue_size=10),
-    'D1': rospy.Publisher('sensor/D1', Int32, queue_size=10),
-    'D2': rospy.Publisher('sensor/D2', Int32, queue_size=10),
+    'A1': rospy.Publisher('sensor/A1', Float32, queue_size=10),
+    'A2': rospy.Publisher('sensor/A2', Float32, queue_size=10),
+    'B1': rospy.Publisher('sensor/B1', Float32, queue_size=10),
+    'B2': rospy.Publisher('sensor/B2', Float32, queue_size=10),
+    'C1': rospy.Publisher('sensor/C1', Float32, queue_size=10),
+    'C2': rospy.Publisher('sensor/C2', Float32, queue_size=10),
+    'D1': rospy.Publisher('sensor/D1', Float32, queue_size=10),
+    'D2': rospy.Publisher('sensor/D2', Float32, queue_size=10),
 }
 
 
 def callback(A1,A2,B1,B2,C1,C2,D1,D2):
     #Goes through all 8 sensors
     #data_array = [A1,A2,B1,B2,C1,C2,D1,D2]
-    data_array = [C1,A1,A2,C2,D2,B2,D1,B1]
-    sensor_names = ['C1','A1','A2','C2','D2','B2','D1','B1']
+    sensor_names = ['A1','A2','B1','B2','C1','C2','D1','D2']
+    data_array = [A1,A2,B1,B2,C1,C2,D1,D2]
 
     min_values_array = []
 
     for l in range(len(data_array)):
-        samples = np.sqrt(np.array(len(data_array[l].ranges), dtype=np.int))
-        samples = np.array(samples, dtype=np.int)
+        
 
-        new_samples = int(np.sqrt(len(data_array[l].ranges)))
+        samples = int(np.sqrt(len(data_array[l].ranges)))
 
         # Avoid unnecessary data conversions
-        new_ranges = np.array(data_array[l].ranges)
+        ranges = np.array(data_array[l].ranges)
         
         # Vectorize operations using numpy functions
-        new_array = np.array([new_ranges[i:i+new_samples] for i in range(0, len(new_ranges), new_samples)])
+        array = np.array([ranges[i:i+samples] for i in range(0, len(ranges), samples)])
         
-        list = []
-        array = []
-        #Arange data in rows and columns
         
-        for j in range(samples):
-            i=j*len(data_array[l].ranges)/samples
-            for i in range(i,i+len(data_array[l].ranges)/samples):        
-                list.append(data_array[l].ranges[i])   
-            array.append(list)
-            list = []
-        print('array', array)
-        print('new array', new_array)
+
         if np.size(array) == 729:
             arranged_array = arrange_4_4(samples,array)
-            array_min = find_min(samples,arranged_array)
-            new_array_min = np.min(arranged_array)
-            print('new array min', new_array_min)
-            print('array min', array_min)
+            array_min = np.min(arranged_array)
             
-            #write_2_csv(min(array_min))
-            array_min_min = min(array_min)
-            print('array min min', array_min_min)
-            min_values_array.append(array_min_min)
-            pubs[sensor_names[l]].publish(array_min_min)
+            
+            
+            min_values_array.append(array_min)
+            pubs[sensor_names[l]].publish(array_min)
+            
             
         else: pass
     write_2_csv(min_values_array)
+
     
   
-def mask_outside_circle(data, radius):
-    # Ustvarjanje koordinatne mreze
-    y, x = np.ogrid[-radius:radius+1, -radius:radius+1]
-    
-    # Ustvarjanje maske za vrednosti izven kroga
-    mask = x**2 + y**2 > radius**2
-    
-    # Nastavitev vrednosti izven kroga na inf
-    data[mask] = np.inf
+def mask_outside_circle(data):
+    np.putmask(data, mask, np.inf)
     
     return data  
     
@@ -99,7 +85,7 @@ def arrange_4_4(samples,data):
     data_reshaped = np.reshape(data, (27, 27))
 
     # Uporaba maske na preoblikovanih podatkih
-    masked_data = mask_outside_circle(data_reshaped, 13)
+    masked_data = mask_outside_circle(data_reshaped)
 
 
     # Izris heatmap
@@ -108,15 +94,23 @@ def arrange_4_4(samples,data):
     # plt.colorbar() # dodaj barvno skalo
     # plt.title('Heatmap of arranged data')
     # plt.show()
-    arranged_array = []
-    for i in range(4):
-        for m in range(4):
-            for k in range((samples/4)*m,m*samples/4+samples/4):
-                for l in range((samples/4)*i,i*samples/4+samples/4):
-                    #print("K: ",k,"L: ",l)
-                    arranged_array.append(masked_data[l][k])
-    #print('arranged data',np.size(arranged_array))
+    
+     # Reshape the masked data into 4x4 blocks
+    reshaped_masked_data = masked_data.reshape(-1, samples // 4, samples // 4)
+
+    # Flatten the reshaped masked data
+    arranged_array = reshaped_masked_data.flatten()
+
     return arranged_array
+    # arranged_array = []
+    # for i in range(4):
+    #     for m in range(4):
+    #         for k in range((samples/4)*m,m*samples/4+samples/4):
+    #             for l in range((samples/4)*i,i*samples/4+samples/4):
+    #                 #print("K: ",k,"L: ",l)
+    #                 arranged_array.append(masked_data[l][k])
+    # #print('arranged data',np.size(arranged_array))
+    # return arranged_array
     
                 
 #Find min values in all 16 squares
@@ -138,16 +132,22 @@ def find_min(samples,data):
     return array_min   
 
 #Write to csv - change name
+# def write_2_csv(data):
+#     cas = datetime.now()
+#     with open(rospack.get_path('ur5-joint-position-control')+'/src/src/my_code/lidar_data/lidar_data.csv', 'a') as file:
+#         writer = csv.writer(file)
+#         # Add the timestamp to the start of the data list
+#         data.insert(0, cas)
+#         # Write the data list to the CSV file
+#         writer.writerow(data)
 def write_2_csv(data):
-    filename = rospack.get_path('ur5-joint-position-control')+'/src/src/my_code/lidar_data/lidar_data.csv'
+    filename = rospack.get_path('ur5-joint-position-control')+'/src/src/my_code/lidar_data/simulated_data.csv'
     file_exists = os.path.isfile(filename)
     cas = datetime.now()
     with open(filename, 'a') as file:
         writer = csv.writer(file)
         if not file_exists:
-            # Assuming data is a list of values, we generate column names as 'Value1', 'Value2', etc.
-            column_names = ['Timestamp'] + ['Value'+str(i+1) for i in range(len(data))]
-            writer.writerow(column_names)
+            writer.writerow(['Timestamp', 'A1', 'A2', 'C1', 'C2', 'B1', 'B2', 'D1', 'D2'])
         # Add the timestamp to the start of the data list
         data.insert(0, cas)
         # Write the data list to the CSV file
