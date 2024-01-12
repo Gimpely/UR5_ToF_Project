72e50f4efd60b1df30eb2465879ad661fc9c42df
Jakob Gimpelj <22889949+Gimpely@users.noreply.github.com>
2024-01-12 14:22:17 +0100

    Add files via upload

--- LiveGraph.py
+++ LiveGraph.py
@@ -24,12 +24,13 @@ for sensor in data_real.keys():
 # Function to update the plot
 def update_plot():
     plt.ion()  # Turn on interactive mode
+    num_points = 20
     while not rospy.is_shutdown():
         for i, sensor in enumerate(data_real.keys(), start=1):
             plt.subplot(4, 2, i)  # Adjust as needed
             plt.clf()  # Clear the current figure
             if len(data_real[sensor]) > 0 and len(data_sim[sensor]) > 0:
-                plt.plot([real - sim for real, sim in zip(data_real[sensor], data_sim[sensor])], label=f'{sensor} Difference')
+                 plt.plot([real - sim for real, sim in zip(data_real[sensor][-num_points:], data_sim[sensor][-num_points:])], label=f'{sensor} Difference')
                 plt.legend()
         plt.pause(0.01)  # Pause for a short period to allow the plot to update
 
